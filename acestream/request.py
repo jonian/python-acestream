f281afb3e25beff6409cdcc0b38073163519a157
Jonian Guveli <jonian@hardpixel.eu>
2018-11-08 00:08:50 +0200

    request: rename getsearch to getapi

--- acestream/request.py
+++ acestream/request.py
@@ -29,11 +29,11 @@ class Request(object):
   def getversion(self):
     return self.getservice(method='get_version')
 
-  def getsearch(self, **params):
-    return self.get('server/api', **params, format='json')
+  def getapi(self, **params):
+    return self.get('server/api', **params)
 
   def gettoken(self):
-    return self.getsearch(method='get_api_access_token')
+    return self.getapi(method='get_api_access_token')
 
   def getstream(self, **params):
     return self.get('ace/getstream', **params, format='json')
