dd4a5c36a29f8a86a22e4e8c173ab18b20c5daaa
Jonian Guveli <jonian@hardpixel.eu>
2018-11-07 23:12:00 +0200

    request: remove success_key parameter
    use response and result as keys to return response

--- acestream/request.py
+++ acestream/request.py
@@ -20,14 +20,11 @@ class Request(object):
     self.base = '{0}://{1}:{2}'.format(schema, host, port)
 
   def get(self, url, **params):
-    getkey = params.pop('success_key', 'response')
     apiurl = self._geturl(url, **params)
-
-    return self._request(apiurl, getkey)
+    return self._request(apiurl)
 
   def getservice(self, **params):
-    apiurl = 'webui/api/service'
-    return self.get(apiurl, **params, format='json', success_key='result')
+    return self.get('webui/api/service', **params, format='json')
 
   def getversion(self):
     return self.getservice(method='get_version')
@@ -41,19 +38,19 @@ class Request(object):
 
     return '{0}/{1}?{2}'.format(self.base, apiurl, params)
 
-  def _request(self, url, success_key):
+  def _request(self, url):
     try:
       response = request.urlopen(url).read()
-      return self._generate_response(response, success_key)
+      return self._generate_response(response)
     except (ConnectionRefusedError, URLError):
       return Response(error='noconnect', message='engine unavailable')
 
-  def _generate_response(self, output, success_key):
-    output   = parse_json(output)
-    error    = output.get('error', 'content unavailable')
-    response = output.get(success_key, False)
+  def _generate_response(self, output):
+    output = parse_json(output)
+    error  = output.get('error', 'content unavailable')
+    result = output.get('result') or output.get('response')
 
-    if response:
-      return Response(data=response)
+    if result:
+      return Response(data=result)
     else:
-      return Response(error='unavailable', message=error)
+      return Response(message=error, error='unavailable')
