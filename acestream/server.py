45be58068b79e534f27e629adeb99ac87cc498d1
Jonian Guveli <jonian@hardpixel.eu>
2018-11-14 15:28:41 +0200

    server: extract request functions into class

--- acestream/server.py
+++ acestream/server.py
@@ -28,7 +28,7 @@ class Response(object):
       return message
 
 
-class Server(object):
+class Request(object):
 
   def __init__(self, host, port=6878, schema='http'):
     self.base = self._geturl_base(schema, host, port)
@@ -36,47 +36,6 @@ class Server(object):
   def get(self, url, **params):
     apiurl = self._geturl(url, **params)
     return self._request(apiurl)
-
-  def getservice(self, **params):
-    return self.get('webui/api/service', format='json', **params)
-
-  def getversion(self):
-    return self.getservice(method='get_version')
-
-  def getserver(self, **params):
-    return self.get('server/api', **params)
-
-  def gettoken(self):
-    return self.getserver(method='get_api_access_token')
-
-  def getsearch(self, **params):
-    return self.getserver(method='search', token=self.token, **params)
-
-  def getstream(self, **params):
-    if params.pop('hls', False):
-      return self.get('ace/manifest.m3u8', format='json', **params)
-    else:
-      return self.get('ace/getstream', format='json', **params)
-
-  def getbroadcast(self, manifest_url):
-    return self.get('hls/manifest.m3u8', format='json', manifest_url=manifest_url)
-
-  @property
-
-  def version(self):
-    response = self.getversion()
-    return self._get_response_key(response, 'version')
-
-  @property
-
-  def available(self):
-    return bool(self.version)
-
-  @property
-
-  def token(self):
-    response = self.gettoken()
-    return self._get_response_key(response, 'token')
 
   def _geturl(self, path, **params):
     params = dict(map(self._parse_param, params.items()))
@@ -119,3 +78,47 @@ class Server(object):
       value = int(value)
 
     return (key, value)
+
+
+class Server(Request):
+
+  def getservice(self, **params):
+    return self.get('webui/api/service', format='json', **params)
+
+  def getversion(self):
+    return self.getservice(method='get_version')
+
+  def getserver(self, **params):
+    return self.get('server/api', **params)
+
+  def gettoken(self):
+    return self.getserver(method='get_api_access_token')
+
+  def getsearch(self, **params):
+    return self.getserver(method='search', token=self.token, **params)
+
+  def getstream(self, **params):
+    if params.pop('hls', False):
+      return self.get('ace/manifest.m3u8', format='json', **params)
+    else:
+      return self.get('ace/getstream', format='json', **params)
+
+  def getbroadcast(self, manifest_url):
+    return self.get('hls/manifest.m3u8', format='json', manifest_url=manifest_url)
+
+  @property
+
+  def version(self):
+    response = self.getversion()
+    return self._get_response_key(response, 'version')
+
+  @property
+
+  def available(self):
+    return bool(self.version)
+
+  @property
+
+  def token(self):
+    response = self.gettoken()
+    return self._get_response_key(response, 'token')
