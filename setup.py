4d6bacdb6583bfd5a70b2ea04ce66fd5de8bfcc9
jonian <jonian@hardpixel.eu>
2021-04-28 17:24:37 +0300

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
   name='acestream',
-  version='0.1.9',
+  version='0.2.0',
   author='Jonian Guveli',
   author_email='jonian@hardpixel.eu',
   description='Interact with the AceStream Engine and the HTTP API',
